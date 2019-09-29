47f6b5735cf1db352c64c964e5affdc553916f5d
shubham860 <shubham.chauhan860@gmail.com>
2019-09-29 17:13:54 +0530

    update

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,8 +1,26 @@
 const BUY_CAKE = 'BUY_CAKE'
-
+//action creater
 function buycake(){
   return {
     type : BUT_CAKE,
     info : 'First redux app'
+  }
+}
+
+// state
+
+const initialstate = {
+  numofcakes : 10
+}
+
+// reducers
+
+const reducer = (state=initialstate,action) => {
+  switch(action.type){
+    case BUY_CAKE : return {
+      ...state,
+      numofcakes = state.numofcakes - 1
+    }
+    default : state
   }
 }
